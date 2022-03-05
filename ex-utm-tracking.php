3933e91bb63f79413361e44854fc02a5716cbec9
JulianWagner <juliantosun@gmail.com>
2022-03-05 10:05:30 +0100

    1.0.8

--- ex-utm-tracking.php
+++ ex-utm-tracking.php
@@ -4,7 +4,7 @@
 Plugin Name: Ex Utm Tracking
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 1.0.7
+Version: 1.0.8
 Author: julian
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
